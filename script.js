978e3050c1208363ec90ea6ec1d0286ead786ab0
Daaanxx <dannxko@gmail.com>
2023-04-20 19:51:24 +0100

    Working on carousel

--- script.js
+++ script.js
@@ -39,3 +39,21 @@ $.ajax(settings).done(function (response) {
 //   .catch(function (error) {
 //     console.error(error);
 //   });
+
+$(document).ready(function){
+  // Activate Carousel
+  $("#myCarousel").carousel();
+
+  // Enable Carousel Indicators
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel").carousel(2);
+  });
+  $(".item4").click(function(){
+    $("#myCarousel").carousel(3);
+  })};
